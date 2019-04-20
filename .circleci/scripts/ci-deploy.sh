63a543c26db19c8d1c572ab40047c46425c1bead
Pablo Fredrikson <pablo.fredrikson@gmail.com>
2019-04-20 17:30:11 -0300

    testing 4

--- .circleci/scripts/ci-deploy.sh
+++ .circleci/scripts/ci-deploy.sh
@@ -10,14 +10,12 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
+envsubst < ./.circleci/kube/deployment.yml.template > ./.circleci/kube/deployment.yml
 
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+echo "$KUBERNETES_KUBECONFIG" | base64 --decode > kubeconfig.yml
 
-./kubectl \
-  --kubeconfig=/dev/null \
-  --server=$KUBERNETES_SERVER \
-  --certificate-authority=cert.crt \
-  --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
+cat kubeconfig.yml
+
+./kubectl --kubeconfig=kubeconfig.yml get nodes
+
+./kubectl --kubeconfig=kubeconfig.yml apply -f ./.circleci/kube
